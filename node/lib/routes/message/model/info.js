e55b384ceb27b351bf13d0b98a12526e301544ff
Jordan Tymburski <hello@jordantymburski.com>
2019-09-23 22:09:59 -0600

    Validation for the send message input model

--- node/lib/routes/message/model/info.js
+++ node/lib/routes/message/model/info.js
@@ -1,3 +1,7 @@
+const EmailValidator = require('email-validator');
+
+const isString = require('lodash/isString');
+
 module.exports = {
   from: from,
   validate: validate
@@ -8,5 +12,26 @@ function from(message) {
 }
 
 function validate(req, res, next) {
-  throw new Error('MessageInfo model not implemented as an input');
+  // from email
+  const from = req.body.from;
+  if (!isString(from) || !EmailValidator.validate(from)) {
+    return next(new HttpError(400,
+                      'from parameter is required and must be a valid email'));
+  }
+
+  // to email
+  const to = req.body.to;
+  if (!isString(to) || !EmailValidator.validate(to)) {
+    return next(new HttpError(400,
+                      'to parameter is required and must be a valid email'));
+  }
+
+  // content
+  const content = req.body.content;
+  if (!content || !isString(content)) {
+    return next(new HttpError(400,
+                      'content parameter is required and must be a valid string'));
+  }
+
+  return next();
 }
--- node/test/message/send.js
+++ node/test/message/send.js
@@ -18,6 +18,7 @@ function request(chai, app, config, authKey) {
 
 function test(chai, app, config) {
   describe('POST / : send message', () => {
+    const goodInput = generateInput();
 
     // Fail on lack of an auth key
     it('it should fail without any auth key', (done) => {
@@ -46,9 +47,122 @@ function test(chai, app, config) {
         });
     });
 
-    // TODO! Full coverage testing
-    it('it should have more tests', (done) => {
-      done(new Error('not implemented'));
+    // No 'from' address
+    it('it should fail on no \'from\' address', (done) => {
+      request(chai, app, config, config.AUTH_KEY)
+        .send({
+          to: goodInput.to,
+          content: goodInput.content
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    // Invalid 'from' address
+    it('it should fail on invalid \'from\' address', (done) => {
+      request(chai, app, config, config.AUTH_KEY)
+        .send({
+          from: 'thisisarealemail@hatsoff',
+          to: goodInput.to,
+          content: goodInput.content
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    // No 'to' address
+    it('it should fail on no \'to\' address', (done) => {
+      request(chai, app, config, config.AUTH_KEY)
+        .send({
+          from: goodInput.from,
+          content: goodInput.content
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    // Invalid 'to' address
+    it('it should fail on invalid \'to\' address', (done) => {
+      request(chai, app, config, config.AUTH_KEY)
+        .send({
+          from: goodInput.from,
+          to: 'woahrealhats@vroom@tomato.com',
+          content: goodInput.content
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    // No content
+    it('it should fail on no \'content\'', (done) => {
+      request(chai, app, config, config.AUTH_KEY)
+        .send({
+          from: goodInput.from,
+          to: goodInput.to
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    // Invalid content
+    it('it should fail on invalid \'content\'', (done) => {
+      request(chai, app, config, config.AUTH_KEY)
+        .send({
+          from: goodInput.from,
+          to: goodInput.to,
+          content: 14239
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    // Empty content
+    it('it should fail on empty \'content\'', (done) => {
+      request(chai, app, config, config.AUTH_KEY)
+        .send({
+          from: goodInput.from,
+          to: goodInput.to,
+          content: ''
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    // It should successfully send
+    it('it should queue to send', (done) => {
+      request(chai, app, config, config.AUTH_KEY)
+        .send(goodInput)
+        .end((err, res) => {
+          res.should.have.status(200);
+          // TODO: More validation if it was added to the queue
+          done();
+        });
     });
   });
+}
+
+// INTERNALS
+
+function generateInput() {
+  let input = {
+    from: randomWords({ min: 2, max: 2, join: '@' }) + ".com",
+    to: randomWords({ min: 2, max: 2, join: '@' }) + ".com",
+    content: randomWords({ min: 12, max: 100, join: ' ' })
+  };
+
+  return input;
 }
